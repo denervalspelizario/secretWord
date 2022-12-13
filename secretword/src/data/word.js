502ee3b303e3cd5270f9112d8f53b727c6b41944
denervalspelizario <denervalspelizario@gmail.com>
2022-12-13 08:33:52 -0300

    criacao de app secret word e adicao de word.js que servira com bd do projeto

---
+++ secretword/src/data/word.js
@@ -0,0 +1,10 @@
+
+
+export const wordList = {
+carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Headset"],
+programacao: ["Linguagem", "Framework", "Javascript", "React", "Java"],
+alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
