68967448db6151087fa7b01259af5394df55f158
denervalspelizario <denervalspelizario@gmail.com>
2022-12-15 07:37:53 -0300

    update app secretword criacao de componentes game e gamover e suas importacoes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import StartScreen from './components/StartScreen';
+import { useCallback, useState, useEffect } from 'react';
+import {wordList} from './data/word'
 import './App.css';
 
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
+
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <StartScreen/>}
+      {gameStage === 'end' && <StartScreen/>}
     </div>
   );
 }
