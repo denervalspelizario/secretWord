1301f0a176b2ab6954b2fb9459421e1bc94bffde
denervalspelizario <denervalspelizario@gmail.com>
2022-12-18 07:30:52 -0300

    update app secret word criacao de evento insercao de letra e renovacao de de campo para nova letra

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,22 @@
 import './Game.css';
+import { useState, useRef } from 'react';
 
 const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score,}) => {
+
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+
+    setLetter("");
+
+    letterInputRef.current.focus();
+  }
+
   return (
     <div className="game">
       <p className="points">
@@ -23,8 +39,8 @@ const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters
       </div>
       <div className="letterContainer">
         <p>Tente adivinha a letra da palavra:</p>
-        <form>
-          <input type="text" name='letter' maxLength="1" required/>
+        <form onSubmit={handleSubmit} >
+          <input type="text" name='letter' maxLength="1" required  onChange={(e) => setLetter(e.target.value)}  value={letter} ref={letterInputRef} />
           <button>Jogar!</button>
         </form>
       </div>
